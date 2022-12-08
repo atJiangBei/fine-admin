ca45431e57c528591e6185da747e8967609a91d8
jiang-bei <792689433@qq.com>
2022-12-09 00:00:14 +0800

    update 2022.12.09.00:00

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,18 +1,41 @@
 <template>
-  <div>
-    <a-row>
-      <a-col :span="12">
-        <div ref="pie1" style="height: 400px"></div>
-      </a-col>
-      <a-col :span="12">
-        <div ref="pie2" style="height: 400px"></div>
-      </a-col>
-    </a-row>
-    <a-row>
-      <a-col :span="24">
-        <div ref="map" style="height: 540px"></div>
-      </a-col>
-    </a-row>
+  <div class="margin-default">
+    <div>
+      <a-row :gutter="10" wrap>
+        <a-col :span="8">
+          <div class="padding-default default-content-bg">
+            <div ref="pie1" style="height: 400px"></div>
+          </div>
+        </a-col>
+        <a-col :span="8">
+          <div class="padding-default default-content-bg">
+            <div ref="pie2" style="height: 400px"></div>
+          </div>
+        </a-col>
+        <a-col :span="8">
+          <div class="padding-default default-content-bg">
+            <div ref="category0" style="height: 400px"></div>
+          </div>
+        </a-col>
+      </a-row>
+      <div class="filler-row-default-x"></div>
+      <a-row :gutter="10" wrap>
+        <a-col :span="12">
+          <div class="padding-default default-content-bg">
+            <div ref="map" style="height: 540px"></div>
+          </div>
+        </a-col>
+        <a-col :span="12">
+          <div class="padding-default default-content-bg">
+            <div ref="category1" style="height: 540px"></div>
+          </div>
+        </a-col>
+      </a-row>
+      <div class="filler-row-default-x"></div>
+      <div class="padding-default default-content-bg">
+        <div ref="category" style="height: 540px"></div>
+      </div>
+    </div>
   </div>
 </template>
 <script lang="ts">
@@ -22,7 +45,14 @@ import {
   usePermissionStoreHook,
 } from '@/store/modules/permission';
 import * as Echarts from 'echarts';
-import { directOption, pieOption1, pieOption2 } from './useData';
+import {
+  directOption,
+  pieOption1,
+  pieOption2,
+  categoryOption0,
+  categoryOption1,
+  categoryOption,
+} from './useData';
 
 const initEcharts = (dom: HTMLElement, option: any) => {
   const echartsMap = Echarts.init(dom);
@@ -34,11 +64,17 @@ export default defineComponent({
     const map = ref();
     const pie1 = ref();
     const pie2 = ref();
+    const category0 = ref();
+    const category1 = ref();
+    const category = ref();
     const maps: any = [];
     onMounted(() => {
       maps.push(initEcharts(pie1.value, pieOption1));
       maps.push(initEcharts(pie2.value, pieOption2));
       maps.push(initEcharts(map.value, directOption));
+      maps.push(initEcharts(category0.value, categoryOption0));
+      maps.push(initEcharts(category1.value, categoryOption1));
+      maps.push(initEcharts(category.value, categoryOption));
       window.addEventListener('resize', resize);
     });
     const resize = () => {
@@ -47,7 +83,7 @@ export default defineComponent({
     onBeforeUnmount(() => {
       window.removeEventListener('resize', resize);
     });
-    return { map, pie1, pie2 };
+    return { map, pie1, pie2, category0, category1, category };
   },
 });
 </script>
